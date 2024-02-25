ea3b0274202b31e3513e0e23ec242eeb0785c8a2
Yeonsu1027 <rito1205@naver.com>
2024-02-26 01:55:57 +0900

    수정

--- animaldic/models/tbl_notice.js
+++ animaldic/models/tbl_notice.js
@@ -2,35 +2,51 @@ import { Model } from "sequelize";
 
 export default class tbl_notice extends Model {
   static init(sequelize, DataTypes) {
-    return super.init(
+  return super.init({
+    n_num: {
+      autoIncrement: true,
+      type: DataTypes.BIGINT,
+      allowNull: false,
+      primaryKey: true
+    },
+    n_author: {
+      type: DataTypes.STRING(15),
+      allowNull: false
+    },
+    n_title: {
+      type: DataTypes.STRING(150),
+      allowNull: false
+    },
+    n_content: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    n_image_name: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    n_image_origin_name: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    n_date: {
+      type: DataTypes.STRING(10),
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    tableName: 'tbl_notice',
+    timestamps: false,
+    indexes: [
       {
-        n_num: {
-          type: DataTypes.STRING(100),
-          allowNull: true,
-        },
-        n_contents: {
-          type: DataTypes.STRING(200),
-          allowNull: true,
-        },
-        n_director: {
-          type: DataTypes.STRING(20),
-          allowNull: false,
-          primaryKey: true,
-        },
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "n_num" },
+        ]
       },
-      {
-        sequelize,
-        tableName: "tbl_notice",
-        timestamps: false,
-        indexes: [
-          {
-            name: "PRIMARY",
-            unique: true,
-            using: "BTREE",
-            fields: [{ name: "n_director" }],
-          },
-        ],
-      }
-    );
+    ]
+  });
   }
 }
--- animaldic/routes/notice.js
+++ animaldic/routes/notice.js
@@ -1,14 +1,50 @@
 import express from "express";
 import DB from "../models/index.js";
+import moment from "moment";
+import { upLoad } from "../modules/file_upload.js";
+
+
 const NOTICE = DB.models.tbl_notice;
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("menu/notice");
-});
+router.get("/", async (req, res) => {
 
-router.get("/nwrite", (req, res) => {
-  res.render("menu/nwrite");
+  const row = await NOTICE.findAll();
+
+  res.render("menu/notice/notice",{row});
 });
+// ----------------------------- 작성
+router.get("/insert", async (req,res)=>{
+const manager = req.session.user ? req.session.user.m_role : undefined;
+  if (manager === "ADMIN") {
+
+    res.render("menu/notice/insert")
+  }
+  else {
+    res.redirect("/"); // 로그인안했으면 못들어가게
+  }
+})
+
+router.post("/insert", upLoad.single("n_image"), async (req,res)=>{
+  const manager = req.session.user ? req.session.user.m_username : undefined;
+  req.body.n_date= moment().format("YYYY-MM-DD");
+  req.body.n_author = manager;
+
+  const file = req.file;
+
+  if (file) {
+    req.body.n_image_name = file.filename;
+    req.body.n_image_origin_name = file.originalname;
+  }
+  try {
+    await NOTICE.create(req.body);
+    res.redirect("/notice")
+  } catch (error) {
+    // return res.json(error);
+    console.log(error);
+  }
+
+})
+
 
 export default router;
